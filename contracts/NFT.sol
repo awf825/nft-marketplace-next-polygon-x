8ca631f9fb45a9925c9735ce4930535638b65a95
Aiden Flynn <aidenflynn@MacBook-Pro.cable.rcn.com>
2021-12-16 14:08:32 -0500

    init

---
+++ contracts/NFT.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: MIT 
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract NFT is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    /*
+        Counters package creates an incremental unique identifier for each token
+        when first token is minted, id is 1, when second, 2 ... ...
+
+        contractAddress is address of the marketplace that we want to allow the nft to be able to interact with or vice versa.
+        We want to be able to give the nft market the ability to transact these tokens or change the ownership of 
+        these tokens from a separate contract
+    */
+    Counters.Counter private _tokenIds;
+    address contractAddress;
+
+    /*
+        constructor method takes in address as the only arg; when we deploy this contract, we need to pass in
+        the address of the actual marketplace. So we're first deploying the actual market, and then this contract.
+
+        createToken is for minting new tokens. We don;t need to pass much data here because function will come 
+        with metadata about the user, since its a transaction (i.e message.sender identifies the minter/buyer etc)
+        For each tokenId generated, an nft is minted. We return the newItemId for the sake of our client app. We 
+        are going to be minting with this contract, but then selling the nft in a separate transaction. 
+    */
+
+    constructor(address marketplaceAddress) ERC721("Metaverse Tokens", "METT") {
+        contractAddress = marketplaceAddress;
+    }
+
+    function createToken(string memory tokenURI) public returns (uint) {
+        _tokenIds.increment();
+        uint256 newItemId = _tokenIds.current();
+
+        _mint(msg.sender, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+        setApprovalForAll(contractAddress, true);
+        return newItemId;
+    }
+
+}
---
+++ hardhat.config.js
@@ -0,0 +1,33 @@
+/* 
+  do we want to interact with public or private RPC endpoints with polygon?
+  https://docs.polygon.technology/docs/develop/network-details/network
+
+  Private key is from my metamask account. This basically allows me to make transactions 
+  on the network.
+*/
+
+require("@nomiclabs/hardhat-waffle");
+const fs = require("fs");
+const privateKey = fs.readFileSync(".secret").toString();
+const projectId = "5ae4b97d4ee44b838e88224cb474d9bf";
+
+module.exports = {
+  // the name of app, config different networks here
+  networks: {
+    hardhat: {
+      // hardhat specific, will inject and use accounts for us when playing locally
+      chainId: 1337
+    },
+    mumbai: {
+      url: `https://polygon-mumbai.infura.io/v3/${projectId}`,
+      accounts: [privateKey]
+    },
+    mainnet: {
+      url: `https://polygon-mainnet.infura.io/v3/${projectId}`,
+      accounts: [privateKey]
+    }
+  },
+  solidity: "0.8.4",
+};
+
+
