244d01c4b57b617034fc739d62cb85c9964a14a1
Aiden Flynn <aidenflynn@MacBook-Pro.cable.rcn.com>
2022-01-19 23:56:35 -0500

    done through roadmap, need to apply infinite scroll and minter image

--- pages/divisions/founders.js
+++ pages/divisions/founders.js
@@ -1,8 +1,67 @@
+import Image from 'next/image';
+
 export default function Founders() {
     return (
         <div className="founders">
-            {/* <Image src="/mint_pic.png" layout="fill"/> */}
-            <p>FOUNDERS</p>
+            <div className="fndr-header">
+                <h1>THE FOUNDERS</h1>
+            </div>
+            <div className="fndr-row">
+                <div className="fndr-text">
+                    <h3 className="fndr-subheader">JOHNNY VAN GOGH (THE ARTIST)</h3>
+                    <ul className="fndr-ul">
+                        <li>23+ years of art experience</li>
+                        <li>Prefers rom-coms to action movies</li>
+                        <li>Favorite Drink: Soda Water</li>
+                    </ul>
+                    <h4 className="fndr-discord">DISCORD: JohnnyVG<span style= {{ letterSpacing: '0.3em' }}>#3661</span></h4>
+                </div>
+                <div className="fndr-img">
+                    <Image src="/B13.png" alt="tv-white" width="350" height="350" />
+                </div>
+            </div>
+            <div className="fndr-row">
+                <div className="fndr-text">
+                    <h3 className="fndr-subheader">JOHNNY VAN GOGH (THE ARTIST)</h3>
+                    <ul className="fndr-ul">
+                        <li>23+ years of art experience</li>
+                        <li>Prefers rom-coms to action movies</li>
+                        <li>Favorite Drink: Soda Water</li>
+                    </ul>
+                    <h4 className="fndr-discord">DISCORD: JohnnyVG<span style= {{ letterSpacing: '0.3em' }}>#3661</span></h4>
+                </div>
+                <div className="fndr-img">
+                    <Image src="/B13.png" alt="tv-white" width="350" height="350" />
+                </div>
+            </div>
+            <div className="fndr-row">
+                <div className="fndr-text">
+                    <h3 className="fndr-subheader">JOHNNY VAN GOGH (THE ARTIST)</h3>
+                    <ul className="fndr-ul">
+                        <li>23+ years of art experience</li>
+                        <li>Prefers rom-coms to action movies</li>
+                        <li>Favorite Drink: Soda Water</li>
+                    </ul>
+                    <h4 className="fndr-discord">DISCORD: JohnnyVG<span style= {{ letterSpacing: '0.3em' }}>#3661</span></h4>
+                </div>
+                <div className="fndr-img">
+                    <Image src="/B13.png" alt="tv-white" width="350" height="350" />
+                </div>
+            </div>
+            <div className="fndr-row">
+                <div className="fndr-text">
+                    <h3 className="fndr-subheader">JOHNNY VAN GOGH (THE ARTIST)</h3>
+                    <ul className="fndr-ul">
+                        <li>23+ years of art experience</li>
+                        <li>Prefers rom-coms to action movies</li>
+                        <li>Favorite Drink: Soda Water</li>
+                    </ul>
+                    <h4 className="fndr-discord">DISCORD: JohnnyVG<span style= {{ letterSpacing: '0.3em' }}>#3661</span></h4>
+                </div>
+                <div className="fndr-img">
+                    <Image src="/B13.png" alt="tv-white" width="350" height="350" />
+                </div>
+            </div>
         </div>
     )
 }
