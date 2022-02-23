527aca6d5a35139be02e6c7ae9d392179b3e795a
Aiden Flynn <aidenflynn@MacBook-Pro.cable.rcn.com>
2022-02-22 21:52:50 -0500

    final touches to base contract, set max supply using counters library instead

--- contracts/Turtleverse.sol
+++ contracts/Turtleverse.sol
@@ -8,18 +8,72 @@ import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract Turtleverse is ERC721, Ownable, ReentrancyGuard {
+    string private _baseTokenURI;
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
-    // using EnumerableSet for EnumerableSet.AddressSet;
-    // EnumerableSet.AddressSet private _whitelist;
-    // event AddedToWhitelist(address indexed _address);
-    // event RemovedFromWhitelist(address indexed _address);
 
-    string private _baseTokenURI;
-    uint256 priceToMint = .05 ether;
+    using EnumerableSet for EnumerableSet.AddressSet;
 
-    // uint16 public addToAllowListLimit;
-    // uint16 public removeFromAllowListLimit;
+    EnumerableSet.AddressSet private _giveawayList;
+    event AddedToGiveawayList(address indexed _address);
+    event RemovedFromGiveawayList(address indexed _address);
+
+    EnumerableSet.AddressSet private _whitelist;
+    event AddedToWhitelist(address indexed _address);
+    event RemovedFromWhitelist(address indexed _address);
+    
+    uint256 public giveawayPriceToMint = 0 ether;
+    uint256 public presalePriceToMint = .025 ether;
+    uint256 public priceToMint = .05 ether;
+
+    bool public giveawayActive;
+    bool public presaleActive;
+    bool public saleActive;
+
+    uint8 public presaleLimit;
+    mapping(address => uint) public presalePurchasedAmount;  /* max of 3 for whitelist */
+
+    event GiveawayStart(uint256 indexed _giveawayStartTime);
+    event GiveawayPaused(uint256 indexed _giveawayStopTime);
+    event PresaleStart(uint256 indexed _presaleStartTime);
+    event PresalePaused(uint256 indexed _presaleStopTime);
+    event SaleStart(uint256 indexed _saleStartTime);
+    event SalePaused(uint256 indexed _saleStopTime);
+
+    modifier whenGiveawayActive() {
+        require(giveawayActive, "Giveaway not active");
+        _;
+    }
+
+    modifier whenGiveawayPaused() {
+        require(!giveawayActive, "Giveaway not paused");
+        _;
+    }
+
+    modifier whenPresaleActive() {
+        require(presaleActive, "Presale is not active");
+        _;
+    }
+
+    modifier whenPresalePaused() {
+        require(!presaleActive, "Presale is not paused");
+        _;
+    }
+
+    modifier whenSaleActive() {
+        require(saleActive, "Sale is not active");
+        _;
+    }
+
+    modifier whenSalePaused() {
+        require(!saleActive, "Sale is not paused");
+        _;
+    }
+
+    modifier whenAnySaleActive() {
+        require(giveawayActive || presaleActive || saleActive, "Any sale is terminated");
+        _;
+    }
 
     constructor(
         string memory name_,
@@ -29,8 +83,87 @@ contract Turtleverse is ERC721, Ownable, ReentrancyGuard {
         _baseTokenURI = baseURI_;
     }
 
-    function getPriceToMint() public view returns (uint256) {
-        return priceToMint;
+    function addToWhitelist(address[] memory addresses) external onlyOwner {
+        require(addresses.length <= 2000, "Whitelist cannot exceed 2000");
+        for(uint index = 0; index < addresses.length; index+=1) {
+            if (_whitelist.add(addresses[index])) {
+                emit AddedToWhitelist(addresses[index]);
+            }
+        }
+    }
+
+    function removeFromWhitelist(address[] memory addresses) external onlyOwner {
+        require(addresses.length <= 2000, "Whitelist cannot exceed 2000");
+        for(uint index = 0; index < addresses.length; index+=1) {
+            if (_whitelist.remove(addresses[index])) {
+                emit RemovedFromWhitelist(addresses[index]);
+            }
+        }
+    }
+
+    function addToGiveawayList(address[] memory addresses) external onlyOwner {
+        for(uint index = 0; index < addresses.length; index+=1) {
+            if (_giveawayList.add(addresses[index])) {
+                emit AddedToGiveawayList(addresses[index]);
+            }
+        }
+    }
+
+    function purgeGiveawayList() external onlyOwner {
+        delete _giveawayList;
+    }
+
+    function inWhitelist(address value) public view returns (bool) {
+        return _whitelist.contains(value);
+    }
+
+    function inGiveawayList(address value) public view returns (bool) {
+        return _giveawayList.contains(value);
+    }
+    /* giveaway */
+    function startGiveaway() external onlyOwner whenGiveawayPaused whenPresalePaused whenSalePaused {
+        giveawayActive = true;
+        emit GiveawayStart(block.timestamp);
+    }
+
+    function pauseGiveaway() external onlyOwner whenPresalePaused whenSalePaused whenGiveawayActive {
+        giveawayActive = false;
+        emit GiveawayPaused(block.timestamp);
+    }
+
+    /* presale */
+
+    function startPresale(uint8 presaleLimit_) external onlyOwner whenGiveawayPaused whenPresalePaused whenSalePaused {
+        presaleLimit = presaleLimit_;
+        presaleActive = true;
+        emit PresaleStart(block.timestamp);
+    }
+
+    function pausePresale() external onlyOwner whenGiveawayPaused whenSalePaused whenPresaleActive {
+        presaleActive = false;
+        emit PresalePaused(block.timestamp);
+    }
+
+    /* real sale */ 
+
+    function startPublicSale() external onlyOwner whenGiveawayPaused whenPresalePaused whenSalePaused {
+        saleActive = true;
+        emit SaleStart(block.timestamp);
+    }
+
+    function pausePublicSale() external onlyOwner whenGiveawayPaused whenPresalePaused whenSaleActive {
+        saleActive = false;
+        emit SalePaused(block.timestamp);
+    }
+
+    function price() external view returns (uint256) {
+        if (giveawayActive) {
+            return giveawayPriceToMint;
+        } else if (presaleActive) {
+            return presalePriceToMint;
+        } else {
+            return priceToMint;
+        }
     }
 
     function _processMint(address recipient) internal returns (uint256) {
@@ -40,28 +173,35 @@ contract Turtleverse is ERC721, Ownable, ReentrancyGuard {
         return newItemId;
     }
 
-    function mintTokens(uint256 tokensAmount) external payable nonReentrant returns (uint256[] memory) {
-        /* apply the business rules later */
-        // _preValidatePurchase(tokensAmount);
+    function _preValidatePurchase(uint256 tokensAmount) internal view {
+        require(msg.sender != address(0));
+        require(tokensAmount > 0, "Must mint at least one token");
+        //require(totalSupply() + tokensAmount <= publicLimit(), "DA: Minting would exceed max supply");
+        if (giveawayActive) {
+            require(inGiveawayList(msg.sender), "Address isn't whitelisted");
+        } else if (presaleActive) {
+            require(inWhitelist(msg.sender), "Address isn't whitelisted");
+            require(tokensAmount + presalePurchasedAmount[msg.sender] <= presaleLimit, "Presale, limited amount of tokens");
+            require(presalePriceToMint * tokensAmount <= msg.value, "Presale, insufficient funds");
+        } else {
+            require(priceToMint * tokensAmount <= msg.value, "Insufficient funds");
+        }
+    }
 
+    function mintTokens(uint256 tokensAmount) external payable whenAnySaleActive nonReentrant returns (uint256[] memory) {
+        _preValidatePurchase(tokensAmount);
         uint256[] memory tokens = new uint256[](tokensAmount);
         for (uint index = 0; index < tokensAmount; index += 1) {
             tokens[index] = _processMint(msg.sender);
         }
-
-        // if (presaleActive) {
-        //     presalePurchasedAmount[msg.sender] += tokensAmount;
-        // }
-
+        if (presaleActive) {
+            presalePurchasedAmount[msg.sender] += tokensAmount;
+        }
         return tokens;
     }
 
     function withdraw(address payable wallet, uint256 amount) external onlyOwner {
         require(amount <= address(this).balance);
         wallet.transfer(address(this).balance);
-    }
-
-    function getBalance() public view returns (uint256) {
-        return address(this).balance;
     }
 }
